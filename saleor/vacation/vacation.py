0057f0aeef34ec5aa60ac2976bf2a53b9602f3dc
carstvaartjes@gmail.com <JunoAdmin76>
2018-04-07 12:42:56 +0200

    vacations

--- saleor/vacation/vacation.py
+++ saleor/vacation/vacation.py
@@ -3,4 +3,6 @@ import datetime as dt
 vacation_list = \
     [dt.date(2018, 2, 24) + dt.timedelta(days=x) for x in range(0, 4)] + \
     [dt.date(2018, 3, 9) + dt.timedelta(days=x) for x in range(0, 2)] + \
-    [dt.date(2018, 3, 15) + dt.timedelta(days=x) for x in range(0, 3)]
+    [dt.date(2018, 3, 15) + dt.timedelta(days=x) for x in range(0, 3)] + \
+    [dt.date(2018, 4, 26) + dt.timedelta(days=x) for x in range(0, 3)] + \
+    [dt.date(2018, 4, 20) + dt.timedelta(days=x) for x in range(0, 2)]
