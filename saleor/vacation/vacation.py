79d6afcb3f5ebd0a7b33893bc0524117daf1a777
carstvaartjes@gmail.com <JunoAdmin76>
2018-06-09 22:27:22 +0200

    vacations

--- saleor/vacation/vacation.py
+++ saleor/vacation/vacation.py
@@ -5,6 +5,7 @@ vacation_list = \
     [dt.date(2018, 7, 3) + dt.timedelta(days=x) for x in range(0, 1)] + \
     [dt.date(2018, 7, 11) + dt.timedelta(days=x) for x in range(0, 1)] + \
     [dt.date(2018, 7, 14) + dt.timedelta(days=x) for x in range(0, 1)] + \
+    [dt.date(2018, 7, 23) + dt.timedelta(days=x) for x in range(0, 8)] + \
     [dt.date(2018, 8, 24) + dt.timedelta(days=x) for x in range(0, 9)] + \
     [dt.date(2018, 7, 11) + dt.timedelta(days=x) for x in range(0, 1)] + \
     [dt.date(2018, 9, 7) + dt.timedelta(days=x) for x in range(0, 1)] + \
